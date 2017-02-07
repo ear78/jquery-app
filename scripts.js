b57ad2f697090d9a9ed748e5ab4fbfbcf49f9dfb
elliot richardson <ear78@aol.com>
2017-02-06 23:21:10 -0600

    added todo functionality

--- scripts.js
+++ scripts.js
@@ -24,12 +24,38 @@ $(document).ready(function(){
     $('#showForm').on('click', function(){
         $newItemButton.hide();
         $newItemForm.show();
+    });
+
+    // ADDING A NEW LIST ITEMS
+    $newItemForm.on('submit', function(e){
+        e.preventDefault();
+        var text = $('input:text').val();
+        $list.append('<li>' + text + '</li>');
+        $('input:text').val('');
+        updateCount();
+    });
+
+    //CLICK HANDLING FOR DELEGATION ON <UL> ELEMENT
+    $list.on('click', 'li', function(){
+        var $this = $(this);
+        var complete = $this.hasClass('complete');
+
+        if(complete === true) {
+            $this.animate({
+                opacity: 0.0,
+                paddingLeft: '+=180'
+            },500, 'swing', function(){
+                $this.remove();
+            });
+        } else {
+            item = $this.text();
+            $this.remove();
+            $list
+                .append('<li class=\"complete\">' + item + '</li>')
+                .hide().fadeIn(3000);
+            updateCount();
+        }
     })
-
-
-
-
-
 
 
 
